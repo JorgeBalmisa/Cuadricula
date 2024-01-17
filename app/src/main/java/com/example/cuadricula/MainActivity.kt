6bf68c86074164de1ef651bca75a20544fcac43a
JorgeBalmisa <palomaclase41@gmail.com>
2024-01-17 15:01:19 +0100

    Finisheado

--- app/src/main/java/com/example/cuadricula/MainActivity.kt
+++ app/src/main/java/com/example/cuadricula/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -32,6 +34,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cuadricula.data.Datasource
 import com.example.cuadricula.ui.theme.CuadriculaTheme
 import java.time.format.TextStyle
 
@@ -46,6 +49,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    TopicList(
+                        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                    )
                 }
             }
         }
@@ -100,14 +106,24 @@ fun TopicCard(topic : com.example.cuadricula.model.Topic, modifier : Modifier = 
     }
 }
 
-fun TopicList(modifier : Modifier = Modifier) {
+@Composable
 
+fun TopicList(modifier : Modifier = Modifier) {
+LazyVerticalGrid(columns = GridCells.Fixed(2),
+    verticalArrangement = Arrangement.spacedBy(8.dp),
+    horizontalArrangement = Arrangement.spacedBy(8.dp),
+modifier = modifier
+.padding(all = 8.dp)) {
+    items(Datasource.DataSource.topics) { topic ->
+        TopicCard(topic)
+    }
+}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CuadriculaTheme {
-
-    }
+    TopicList(
+        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+    )
 }
